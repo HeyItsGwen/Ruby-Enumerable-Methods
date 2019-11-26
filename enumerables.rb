eaa63e6a2b2fbfd711450494aabdd0ed3ced20fe
Gwen <itsgwenhey@gmail.com>
2019-11-26 08:34:49 -0600

    basic blocks up to my_select
    make some basic methods that just take block, up to my_select

--- enumerables.rb
+++ enumerables.rb
@@ -1,5 +1,30 @@
 # frozen_string_literal: true
 
-module Enumerables
-  def my_each; end
+module Enumerable
+
+  def my_each
+    for i in 0...self.length
+      yield( self[i] )
+    end
+  end
+
+  def my_each_with_index
+    for i in 0...self.length
+      yield( self[i], i)
+    end
+  end
+
+  def my_select
+    returnArr = []
+    self.my_each{|x| returnArr << x if yield(x)}
+    returnArr.nil? ? (return 'nothing') : (return returnArr)
+  end
+      
 end
+
+myarr = [2, 2, 3, 4]
+myWords = ['this', 'that', 'theOther']
+puts 'add'
+puts myarr.my_inject{|x,y| x + y}
+puts 'multiply'
+puts multiply_els(myarr)
