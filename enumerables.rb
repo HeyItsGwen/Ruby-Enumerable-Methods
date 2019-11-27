acff3fc64d01e798c7d3e849f7fcc582bf3472d0
Gwen <itsgwenhey@gmail.com>
2019-11-27 09:38:44 -0600

    rubocop fixes

--- enumerables.rb
+++ enumerables.rb
@@ -1,105 +1,106 @@
 # frozen_string_literal: true
-# rubocop:disable Style/LineLength, Style/RedundantSelf, Style/StringLiterals, Style/For
 
 module Enumerable
-#done
+  # rubocop:disable Style/RedundantSelf, Style/For, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+
   def my_each
     if block_given?
       for i in 0...self.length
-        yield( self [i] )
+        yield(self [i])
       end
     else
       to_enum(:my_each)
     end
   end
-#done
+
   def my_each_with_index
     if block_given?
       for i in 0...self.length
-        yield( self[i], i)
+        yield(self[i], i)
       end
     else
       to_enum(:my_each_with_index)
     end
   end
-#done
+
   def my_select
     if block_given?
-      returnArr = []
-      self.my_each{ |x| returnArr << x if yield(x) }
-      returnArr.nil? ? (return false) : (return returnArr)
+      return_arr = []
+      self.my_each { |x| return_arr << x if yield(x) }
+      return_arr.nil? ? (return false) : (return return_arr)
     else
       to_enum(:my_select)
     end
   end
-#done
+
   def my_all?(pattern = nil)
     if block_given?
-      self.my_select{ |x| yield(x) }.length === self.length ? (return true) : (return false)
+      self.my_select { |x| yield(x) }.length == self.length ? (return true) : (return false)
     elsif !pattern.nil?
       if pattern.is_a?(Regexp)
-        self.my_each{ |x| return false unless pattern.match(x.to_s) }
+        self.my_each { |x| return false unless pattern.match(x.to_s) }
       elsif pattern.is_a?(Class)
-        self.my_each{ |x| return false unless x.is_a?(pattern) }
+        self.my_each { |x| return false unless x.is_a?(pattern) }
       else
-        self.my_each{ |x| return false unless x == pattern }
+        self.my_each { |x| return false unless x == pattern }
       end
     else
-      self.my_each{ |x| return false unless x}
+      self.my_each { |x| return false unless x }
     end
     true
   end
-#done
+
   def my_any?(pattern = nil)
     if block_given?
-      self.my_select{ |x| yield(x) }.length.positive? ? (return true) : (return false)
+      self.my_select { |x| yield(x) }.length.positive? ? (return true) : (return false)
     elsif !pattern.nil?
       if pattern.is_a?(Regexp)
-        self.my_each{ |x| return true if pattern.match(x.to_s) }
+        self.my_each { |x| return true if pattern.match(x.to_s) }
       elsif pattern.is_a?(Class)
-        self.my_each{ |x| return true if x.is_a?(pattern) }
+        self.my_each { |x| return true if x.is_a?(pattern) }
       else
-        self.my_each{ return true if x==pattern }
+        self.my_each { return true if x == pattern }
       end
     else
-      self.my_each{ |x| return true if x }    
+      self.my_each { |x| return true if x }
     end
     false
   end
-#done
+
   def my_none?(pattern = nil)
     if block_given?
-      self.my_select{|x| yield(x)}.length.positive? ? (return false) : (return true)
+      self.my_select { |x| yield(x) }.length.positive? ? (return true) : (return false)
     elsif !pattern.nil?
       if pattern.is_a?(Regexp)
-        self.my_each{ |x| return false if pattern.match(x.to_s)}
+        self.my_each { |x| return false if pattern.match(x.to_s) }
       elsif pattern.is_a?(Class)
-        self.my_each{ |x| return false if x.is_a?(pattern)}
-      else 
-        self.my_each{ |x| return false if x == pat }
+        self.my_each { |x| return false if x.is_a?(pattern) }
+      else
+        self.my_each { |x| return false if x == pat }
       end
     else
-      self.my_each{ |x| return false if x}
+      self.my_each { |x| return false if x }
     end
     true
   end
-#done
+
   def my_count(elem = nil)
     return self.length unless block_given?
+
     if elem.nil?
-      return self.my_select{ |x| yield(x) }.length 
+      return self.my_select { |x| yield(x) }.length
     else
       count = 0
-      self.my_each{ |x| count += 1 if x == elem }
+      self.my_each { |x| count += 1 if x == elem }
       return count
     end
   end
-#done
+
   def my_map
-    unless !block_given? 
-      returnArr = []
-      self.my_each{|x| returnArr << yield(x)}
-      return returnArr
+    if block_given?
+      return_arr = []
+      self.my_each { |x| return_arr << yield(x) }
+      return return_arr
     end
     to_enum(:my_map)
   end
@@ -124,18 +125,9 @@ module Enumerable
     end
     [result, sym]
   end
-      
-end
 
+  # rubocop: enable Style/RedundantSelf, Style/For, Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity
+end
 def multiply_els(arr)
-
-  include Enumerable
-    arr.my_inject(:*)
-
+  arr.my_inject(:*)
 end
-
-myarr = [1, 2, 3, 4]
-myWords = ['this', 'that', 'theOther']
-mystring = 'hello there friends'
-
-puts myarr.my_inject(1){|x,y|x*y}
